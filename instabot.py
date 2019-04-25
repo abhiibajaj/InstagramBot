06922c17f8b02ab4481c838b3d3d7b8d878877c5
Arpit <arpitbajaj98@gmail.com>
2019-04-25 14:26:43 +1000

    Open chrome and put in random username password in instagram login page. Can't find login bbutton yet

--- instabot.py
+++ instabot.py
@@ -2,6 +2,23 @@ from selenium import webdriver
 import os
 import time
 
- class InstagramBot:
+class InstagramBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-     
+        self.driver = webdriver.Chrome("./chromedriver")
+        self.login()
+    
+    def login(self):
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        
+        time.sleep(0.2)
+        self.driver.find_element_by_name("username").send_keys(self.username)
+        self.driver.find_element_by_name("password").send_keys(self.username)
+
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[5]/button/div').click()
+
+
+if __name__ == "__main__":
+    ig_bot = InstagramBot("temp_username", "temp_password")
